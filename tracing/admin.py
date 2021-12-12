cf8cb5e9dbfee7ab5fa76ac47e20d740b83b5949
brianumandap7 <brian.umandap@dotr.gov.ph>
2021-12-12 10:27:11 +0800

    admin.py update

--- tracing/admin.py
+++ tracing/admin.py
@@ -2,10 +2,3 @@ from django.contrib import admin
 
 from .models import Contact_tracing, fever, cold, body_pain, loss_of_taste, loss_of_smell, headache
 # Register your models here.
-admin.site.register(Contact_tracing)
-admin.site.register(fever)
-admin.site.register(cold)
-admin.site.register(body_pain)
-admin.site.register(loss_of_smell)
-admin.site.register(loss_of_taste)
-admin.site.register(headache)
